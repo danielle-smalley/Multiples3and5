64a4c71baf33a1d4f58f6761f4d7dde94ba27a3e
Danielle Smalley <smalley.danielle@gmail.com>
2021-07-26 15:34:32 -0500

    Initial Commit

---
+++ Multiples3And5/Multiples3And5/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3And5
+{
+    public class Class1
+    {
+    }
+}
---
+++ Multiples3And5/Multiples3_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * If we list all the natural numbers below 10
+                that are multiples of 3 or 5, we get 3, 5,
+                6 and 9. The sum of these multiples is 23.
+
+                Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+
+                Remember to commit changes to the repository. Submit your repo url to this assignment. 
+             */
+
+
+
+        }//end main
+    }//end class
+}//end namespace
